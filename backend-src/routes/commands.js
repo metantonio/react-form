4ba89b65b7f2e6605007ded317305c0df1aad7d4
unknown <antonio_martinez88@hotmail.com>
2023-11-20 15:46:11 -0500

    upd

--- backend-src/routes/commands.js
+++ backend-src/routes/commands.js
@@ -5,15 +5,15 @@ router.post('/ls-command', (req, res) => {
     const { directoryName } = req.body;
     //console.log("i'm here")
     // Ejecuta el comando mkdir
-    exec(`mkdir hola`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error: ${stderr}`);
-        return res.status(500).json({ error: 'error looking in directory' });
-      }
-  
-      console.log(`ls: ${stdout}`);
-      res.json({ message: 'succesfully' });
+    exec(`dir`, (error, stdout, stderr) => { //en windowd es dir, en otro sería ls -l
+        if (error) {
+            console.error(`Error: ${stderr}`);
+            return res.status(500).json({ error: 'error looking in directory' });
+        }
+
+        //console.log(`ls: ${stdout}`);
+        res.json({ message: 'succesfully', command: stdout });
     });
-  });
+});
 
 module.exports = router;
