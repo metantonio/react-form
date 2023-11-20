6d2e9d57684c5e7c6f680518132db78fe27c7339
unknown <antonio_martinez88@hotmail.com>
2023-11-20 16:53:38 -0500

    updated

--- src/js/views/tutorial-html/testRunner.jsx
+++ src/js/views/tutorial-html/testRunner.jsx
@@ -2,35 +2,43 @@ import React, { useState } from 'react';
 import TerminalOutput from './terminalOutput.jsx';
 
 const TestRunner = () => {
-  const [lsCommand, setLsCommand] = useState(false);
+    const [lsCommand, setLsCommand] = useState(false);
+    const [loading, setLoading] = useState(false)
 
-  const handleCreateDirectory = async (endpoint) => {
-    try {
-      const response = await fetch(`http://localhost:3002/commands/${endpoint}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify([]),
-      });
+    const handleCreateDirectory = async (endpoint) => {
+        try {
+            setLoading(true)
+            const response = await fetch(`http://localhost:3002/commands/${endpoint}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify([]),
+            });
 
-      const data = await response.json();
-      if(response.ok){
-        setLsCommand(data.command)
-      }
-      console.log(data.message); // Mensaje del servidor
-    } catch (error) {
-      console.error('Error al enviar la solicitud:', error);
-    }
-  };
+            const data = await response.json();
+            if (response.ok) {
+                setLsCommand(data.command)
+            }
+            console.log(data.message); // Mensaje del servidor
+            setLoading(false)
+        } catch (error) {
+            console.error('Error al enviar la solicitud:', error);
+            setLoading(false)
+        }
+    };
 
-  return (
-    <div>
-      <button onClick={()=>handleCreateDirectory('ls-command')}>ls -l</button>
-      <button onClick={()=>handleCreateDirectory('test-1')}>test</button>
-      {lsCommand ? <><TerminalOutput text={lsCommand} /></>: <></>}
-    </div>
-  );
+    return (
+        <div>
+            <button onClick={() => handleCreateDirectory('ls-command')}>ls -l</button>
+            <button onClick={() => handleCreateDirectory('test-1')}>test</button>
+            {loading? <div class="spinner-border text-primary"></div> : <></>}
+            {lsCommand ?
+             <><TerminalOutput text={lsCommand} /></> : 
+             <></>
+             }
+        </div>
+    );
 };
 
 export default TestRunner;
