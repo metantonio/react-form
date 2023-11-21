2243117ace901d4d3da7b3e3fbefdd5c9a81f9e2
unknown <antonio_martinez88@hotmail.com>
2023-11-21 14:53:28 -0500

    updated

--- backend-src/tutorial-html/exercise1.test.js
+++ backend-src/tutorial-html/exercise1.test.js
@@ -6,7 +6,7 @@ const config = {
 let textVariable = process.argv.find(arg => arg.startsWith('--textVariable=')).split('=')[1];
 textVariable = textVariable.slice(1, textVariable.length - 1) //to take out % symbols
 
-test('Exercise 1', () => {
+test('Exercise 01: Hello World', () => {
     //console.log("dentro de la función de test: ", textVariable)
     expect(textVariable).toBe("<span>Hola <strong>Mundo!</strong></span>");
 });
