fb7bddc473f40488a07b30f09c098a3ee4bf3cde
unknown <antonio_martinez88@hotmail.com>
2023-11-21 14:09:38 -0500

    updated

--- src/js/views/tutorial-html/terminalOutput.jsx
+++ src/js/views/tutorial-html/terminalOutput.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 const TerminalOutput = ({ text }) => {
+
+  const terminalRef = useRef(null);
+
+  useEffect(() => {
+    // Desplaza automáticamente hacia abajo para mostrar el final del texto
+    if (terminalRef.current) {
+      terminalRef.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+    }
+  }, [text]);
+
+
   const terminalStyle = {
     backgroundColor: '#2E2E2E',
     color: '#FFFFFF',
@@ -11,8 +22,9 @@ const TerminalOutput = ({ text }) => {
   };
 
   return (
-    <pre style={terminalStyle}>
+    <pre style={terminalStyle} ref={terminalRef}>
       {text}
+      <div ref={terminalRef} />
     </pre>
   );
 };
