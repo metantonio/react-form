58e7c7e696e32f7e7f5cae114aada2ca259ab70a
unknown <antonio_martinez88@hotmail.com>
2023-11-28 11:55:49 -0500

    updated

--- backend-src/routes/commands.js
+++ backend-src/routes/commands.js
@@ -235,7 +235,7 @@ router.post('/unix-commands', (req, res) => {
         else if (commandParts[0] === 'ls' && lesson == 5) {
             childProcess = spawn('ls', [commandParts[1]], { shell: true, cwd: "./unix/home/user1" });
         }
-        else if (data === 'pwd' && lesson == 6) {
+        else if (data === 'pwd' && lesson >= 6 && lesson <=8) {
             childProcess = spawn('pwd', [], { shell: true, cwd: "./unix/home/user1" });
         }
         else if (commandParts[0] === 'ls' && lesson == 6 && commandParts[1] !== '-R') {
