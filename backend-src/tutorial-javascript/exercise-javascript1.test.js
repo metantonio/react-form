02e31d980e54006081f9fbed72845fd8dec214cf
unknown <antonio_martinez88@hotmail.com>
2023-11-29 15:40:23 -0500

    upd

--- backend-src/tutorial-javascript/exercise-javascript1.test.js
+++ backend-src/tutorial-javascript/exercise-javascript1.test.js
@@ -17,7 +17,7 @@ describe('Exercise 01: The Console', () => {
     });
 
     test('Evaluate JavaScript code for printing function', () => {
-        const jsCode = textVariable.replace(/[\n\t\r]+/g, '');
+        const jsCode = textVariable.replace(/[\n\t\r]+\s*/g, '');
         console.log("jsCode:", jsCode)
         try {
             //const func = new Function(jsCode);
