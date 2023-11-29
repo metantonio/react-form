9b1357f2c557dba2282a182ab8d097fac0d63c2a
unknown <antonio_martinez88@hotmail.com>
2023-11-29 14:35:39 -0500

    upd

--- backend-src/tutorial-html/exercise-html1.test.js
+++ backend-src/tutorial-html/exercise-html1.test.js
@@ -2,20 +2,46 @@
 const config = {
     verbose: true,
 };
-console.log('exercise 1 html test');
-let textVariable = process.argv.find(arg => arg.startsWith('--textVariable=')).split('=')[1];
+//console.log('exercise 1 html test');
+let textVariable;
 if (!process.argv.find(arg => arg.startsWith('--textVariable='))) {
     console.error('Error: --textVariable argument is missing.');
     process.exit(1); // Exit the process with an error code
   }
-  
-textVariable = textVariable.slice(1, textVariable.length - 1) //to take out % symbols
 
-test('Exercise 01: Hello World', () => {
+/* test('Exercise 01: Hello World', () => {
     //console.log("dentro de la función de test: ", textVariable)
     expect(textVariable).toMatch(/<span>.*<\/span>/);
     expect(textVariable).toMatch(/<strong>.*<\/strong>/);
     expect(textVariable).toBe("<span>Hola <strong>Mundo!</strong></span>");
+}); */
+
+describe('Exercise 01: Hello World', () => {
+    beforeEach(() => {
+        // Antes de cada prueba, configuramos el valor de textVariable
+        textVariable = process.argv.find(arg => arg.startsWith('--textVariable=')).split('=')[1];
+        textVariable = textVariable.slice(1, textVariable.length - 1); // Para quitar los símbolos de porcentaje
+    });
+
+    test('Contains opening and closing <span> tags', () => {
+        expect(textVariable).toMatch(/<span>.*<\/span>/);
+    });
+
+    test('Contains opening and closing <strong> tags', () => {
+        expect(textVariable).toMatch(/<strong>.*<\/strong>/);
+    });
+
+    test('Contains the exact html code', () => {
+        expect(textVariable).toBe("<span>Hola <strong>Mundo!</strong></span>");
+    });
+
+    /* test('Contains specific text', () => {
+        expect(textVariable).toContain("some specific text");
+    });
+
+    test('Has a length greater than 5', () => {
+        expect(textVariable.length).toBeGreaterThan(5);
+    }); */
 });
 
-export default test;
+export default describe;
