d9a451164be9954ebc713d5ddd00a54c631308e8
unknown <antonio_martinez88@hotmail.com>
2023-12-04 12:08:12 -0500

    upd

---
+++ backend-src/tutorial-javascript/exercise-javascript11.test.js
@@ -0,0 +1,73 @@
+/** @type {import('jest').Config} */
+const config = {
+    verbose: true,
+};
+//console.log('exercise 1 html test');
+let textVariable;
+if (!process.argv.find(arg => arg.startsWith('--textVariable='))) {
+    console.error('Error: --textVariable argument is missing.');
+    process.exit(1); // Exit the process with an error code
+}
+
+describe('Exercise 11: If Statements', () => {
+    beforeEach(() => {
+        // Antes de cada prueba, configuramos el valor de textVariable
+        textVariable = process.argv.find(arg => arg.startsWith('--textVariable=')).split('=').slice(1).join('=');
+        //console.log("textVariable0:", textVariable)
+        textVariable = textVariable.slice(1, textVariable.length - 1); // Para quitar los símbolos de porcentaje
+    });
+
+    //test('Evaluate JavaScript function and its return', () => {        
+    //    let jsCode = textVariable.replace(/[\n\t\r]+\s*/g, '');        
+    //    try {         
+    //        let result = eval(jsCode)
+    //        expect(result).toBe(3);
+    //    } catch (error) {
+    //        console.error("Error during code evaluation:", error);
+    //        throw error;
+    //    }
+    //});    
+
+    test('Evaluate speedometer(101) should return "red"', () => {
+        let jsCode = textVariable.replace(/[\n\t\r]+\s*/g, '');
+        jsCode = jsCode + " console.log(speedometer(101));"
+        try {
+            // capturing the ouput of the console.log
+            let consoleOutput = '';
+            const originalConsoleLog = console.log;
+            console.log = (output) => {
+                consoleOutput += output + '\n';
+            };
+
+            // run code
+            eval(jsCode);
+
+            // Restaurar la función original de console.log
+            console.log = originalConsoleLog;
+
+            // Verificar la presencia de console.log en la salida
+            expect(consoleOutput).toContain(`red`);
+        } catch (error) {
+            console.error("Error during code evaluation:", error);
+            throw error;
+        }
+    });
+
+    /* test('Contains opening and closing <strong> tags', () => {
+        expect(textVariable).toMatch(/<strong>.*<\/strong>/);
+    });
+
+    test('Contains the exact html code', () => {
+        expect(textVariable).toBe("<span>Hola <strong>Mundo!</strong></span>");
+    }); */
+
+    /* test('Contains specific text', () => {
+        expect(textVariable).toContain("some specific text");
+    });
+
+    test('Has a length greater than 5', () => {
+        expect(textVariable.length).toBeGreaterThan(5);
+    }); */
+});
+
+export default describe;
