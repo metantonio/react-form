0979230fc76a256936f9fd70ef84a4cffbfac0c9
unknown <antonio_martinez88@hotmail.com>
2023-12-04 15:00:32 -0500

    updated

---
+++ backend-src/tutorial-javascript/exercise-javascript13.test.js
@@ -0,0 +1,225 @@
+/** @type {import('jest').Config} */
+const config = {
+    verbose: true,
+};
+//console.log('exercise 1 html test');
+let textVariable;
+if (!process.argv.find(arg => arg.startsWith('--textVariable='))) {
+    console.error('Error: --textVariable argument is missing.');
+    process.exit(1); // Exit the process with an error code
+}
+
+describe('Exercise 13: Switch', () => {
+    beforeEach(() => {
+        // Antes de cada prueba, configuramos el valor de textVariable
+        textVariable = process.argv.find(arg => arg.startsWith('--textVariable=')).split('=').slice(1).join('=');
+        //console.log("textVariable0:", textVariable)
+        textVariable = textVariable.slice(1, textVariable.length - 1); // Para quitar los símbolos de porcentaje
+    });
+
+    //test('Evaluate JavaScript function and its return', () => {        
+    //    let jsCode = textVariable.replace(/[\n\t\r]+\s*/g, '');        
+    //    try {         
+    //        let result = eval(jsCode)
+    //        expect(result).toBe(3);
+    //    } catch (error) {
+    //        console.error("Error during code evaluation:", error);
+    //        throw error;
+    //    }
+    //});    
+
+    test('Evaluate pointSystems(0) should return "black"', () => {
+        let jsCode = textVariable.replace(/[\n\t\r]+\s*/g, '');
+        let newNumber = 0;
+        jsCode = jsCode.replace(
+            /console\.log\(speedometer\(\d+\)\);/g,
+            `console.log(pointSystems(${newNumber}));`
+        );
+        try {
+            // capturing the ouput of the console.log
+            let consoleOutput = '';
+            const originalConsoleLog = console.log;
+            console.log = (output) => {
+                consoleOutput += output + '\n';
+            };
+
+            // run code
+            eval(jsCode);
+
+            // Restaurar la función original de console.log
+            console.log = originalConsoleLog;
+
+            // Verificar la presencia de console.log en la salida
+            expect(consoleOutput).toContain(`black`);
+        } catch (error) {
+            console.error("Error during code evaluation:", error);
+            throw error;
+        }
+    });
+
+    test('Evaluate pointSystems(1) should return "yellow"', () => {
+        let jsCode = textVariable.replace(/[\n\t\r]+\s*/g, '');
+        let newNumber = 1;
+        jsCode = jsCode.replace(
+            /console\.log\(speedometer\(\d+\)\);/g,
+            `console.log(pointSystems(${newNumber}));`
+        );
+        try {
+            // capturing the ouput of the console.log
+            let consoleOutput = '';
+            const originalConsoleLog = console.log;
+            console.log = (output) => {
+                consoleOutput += output + '\n';
+            };
+
+            // run code
+            eval(jsCode);
+
+            // Restaurar la función original de console.log
+            console.log = originalConsoleLog;
+
+            // Verificar la presencia de console.log en la salida
+            expect(consoleOutput).toContain(`yellow`);
+        } catch (error) {
+            console.error("Error during code evaluation:", error);
+            throw error;
+        }
+    });
+
+    test('Evaluate pointSystems(2) should return "orange"', () => {
+        let jsCode = textVariable.replace(/[\n\t\r]+\s*/g, '');
+        let newNumber = 0;
+        jsCode = jsCode.replace(
+            /console\.log\(speedometer\(\d+\)\);/g,
+            `console.log(pointSystems(${newNumber}));`
+        );
+        try {
+            // capturing the ouput of the console.log
+            let consoleOutput = '';
+            const originalConsoleLog = console.log;
+            console.log = (output) => {
+                consoleOutput += output + '\n';
+            };
+
+            // run code
+            eval(jsCode);
+
+            // Restaurar la función original de console.log
+            console.log = originalConsoleLog;
+
+            // Verificar la presencia de console.log en la salida
+            expect(consoleOutput).toContain(`orange`);
+        } catch (error) {
+            console.error("Error during code evaluation:", error);
+            throw error;
+        }
+    });
+
+    test('Evaluate pointSystems(3) should return "red"', () => {
+        let jsCode = textVariable.replace(/[\n\t\r]+\s*/g, '');
+        let newNumber = 0;
+        jsCode = jsCode.replace(
+            /console\.log\(speedometer\(\d+\)\);/g,
+            `console.log(pointSystems(${newNumber}));`
+        );
+        try {
+            // capturing the ouput of the console.log
+            let consoleOutput = '';
+            const originalConsoleLog = console.log;
+            console.log = (output) => {
+                consoleOutput += output + '\n';
+            };
+
+            // run code
+            eval(jsCode);
+
+            // Restaurar la función original de console.log
+            console.log = originalConsoleLog;
+
+            // Verificar la presencia de console.log en la salida
+            expect(consoleOutput).toContain(`red`);
+        } catch (error) {
+            console.error("Error during code evaluation:", error);
+            throw error;
+        }
+    });
+
+    test('Evaluate pointSystems(4) should return "green"', () => {
+        let jsCode = textVariable.replace(/[\n\t\r]+\s*/g, '');
+        let newNumber = 0;
+        jsCode = jsCode.replace(
+            /console\.log\(speedometer\(\d+\)\);/g,
+            `console.log(pointSystems(${newNumber}));`
+        );
+        try {
+            // capturing the ouput of the console.log
+            let consoleOutput = '';
+            const originalConsoleLog = console.log;
+            console.log = (output) => {
+                consoleOutput += output + '\n';
+            };
+
+            // run code
+            eval(jsCode);
+
+            // Restaurar la función original de console.log
+            console.log = originalConsoleLog;
+
+            // Verificar la presencia de console.log en la salida
+            expect(consoleOutput).toContain(`green`);
+        } catch (error) {
+            console.error("Error during code evaluation:", error);
+            throw error;
+        }
+    });
+
+    test('Evaluate pointSystems(5) should return "blue"', () => {
+        let jsCode = textVariable.replace(/[\n\t\r]+\s*/g, '');
+        let newNumber = 0;
+        jsCode = jsCode.replace(
+            /console\.log\(speedometer\(\d+\)\);/g,
+            `console.log(pointSystems(${newNumber}));`
+        );
+        try {
+            // capturing the ouput of the console.log
+            let consoleOutput = '';
+            const originalConsoleLog = console.log;
+            console.log = (output) => {
+                consoleOutput += output + '\n';
+            };
+
+            // run code
+            eval(jsCode);
+
+            // Restaurar la función original de console.log
+            console.log = originalConsoleLog;
+
+            // Verificar la presencia de console.log en la salida
+            expect(consoleOutput).toContain(`blue`);
+        } catch (error) {
+            console.error("Error during code evaluation:", error);
+            throw error;
+        }
+    });
+
+
+
+  
+    /* test('Contains opening and closing <strong> tags', () => {
+        expect(textVariable).toMatch(/<strong>.*<\/strong>/);
+    });
+
+    test('Contains the exact html code', () => {
+        expect(textVariable).toBe("<span>Hola <strong>Mundo!</strong></span>");
+    }); */
+
+    /* test('Contains specific text', () => {
+        expect(textVariable).toContain("some specific text");
+    });
+
+    test('Has a length greater than 5', () => {
+        expect(textVariable.length).toBeGreaterThan(5);
+    }); */
+});
+
+export default describe;
