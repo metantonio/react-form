317e267e6e8db459ef5e0b9d029cd78a42ba8b10
unknown <antonio_martinez88@hotmail.com>
2023-12-27 12:08:44 -0500

    updated

--- backend-src/routes/commands.js
+++ backend-src/routes/commands.js
@@ -373,7 +373,7 @@ router.post('/unix-commands', (req, res) => {
             }
             else if (data === 'ls -l; cat hello.txt' && lesson==14) {
                 let resto = commandParts.slice(1)
-                childProcess = spawn('la', ["-l; cat hello.txt"], { shell: true, cwd: "./unix/home/user1" });
+                childProcess = spawn('ls', ["-l; cat hello.txt"], { shell: true, cwd: "./unix/home/user1" });
                 if (lesson === 14) {
                     correcto = true
                 } else {
